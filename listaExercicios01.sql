c80da6d1b9969a6a0c711c1c29a463b1669d9e29
Vinícius Souza Borges <borgesvini11@gmail.com>
2023-08-23 23:40:25 -0300

    exercício 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,6 +1,12 @@
 -- exercício 1
 select titulo from livros;
 
--- exercíco 2
+-- exercício 2
 select NOME from autores
 WHERE nascimento < '1900-01-01';
+
+-- exercício 3
+SELECT titulo 
+from livros 
+inner join autores on autor_id=autores.id
+where NOME = 'J.K. Rowling';
