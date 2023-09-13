c98ada344a3b884311ddd072de48df587feb0c13
Vinícius Borges <103907931+BG0010@users.noreply.github.com>
2023-09-13 11:55:09 -0300

    exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -3,3 +3,14 @@ CREATE PROCEDURE sp_ListarAutores()
 BEGIN
     SELECT * FROM Autor;
 END;
+
+--exercício 2
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo, Autor.Nome, Autor.Sobrenome
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
