f712cecdca1cdfe1a960e3dff4ecb1e04ab8bd02
Vinícius Borges <103907931+BG0010@users.noreply.github.com>
2023-10-04 18:19:10 -0300

    Tamanho de cada nome

--- Funções 4° BIM/funcao.sql
+++ Funções 4° BIM/funcao.sql
@@ -6,3 +6,5 @@ insert into nomes (nome)
 values ('Roberta'), ('Roberto'), ('Maria Clara'), ('João')
 
 select UPPER(nome) as nome_maiusculo from nomes;
+
+select nome, LENGHT(nome) as tamanho from nomes;
