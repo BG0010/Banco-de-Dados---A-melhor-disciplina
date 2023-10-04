25688450eb789eacaa413464e830567cff5f3755
Vinícius Borges <103907931+BG0010@users.noreply.github.com>
2023-10-04 18:08:28 -0300

    Add "Sr" e "Sra" antes dos nomes

--- funcao.sql
+++ funcao.sql
@@ -8,3 +8,5 @@ values ('Roberta'), ('Roberto'), ('Maria Clara'), ('João')
 select UPPER(nome) as nome_maiusculo from nomes;
 
 select nome, LENGHT(nome) as tamanho from nomes;
+
+elect case when nome like "João%" then "Sr. " nome end as nome-com-tratamento from nomes;
