0e799ac69d1c6bd5345fcd25ed5044d54529dc87
Vinícius <borgesvini11@gmail.com>
2023-10-08 11:11:28 -0300

    Adicionar "Sr." e "Sra." antes dos nomes masculinos e femininos

--- Funções 4° BIM/funcao.sql
+++ Funções 4° BIM/funcao.sql
@@ -8,3 +8,10 @@ values ('Roberta'), ('Roberto'), ('Maria Clara'), ('João')
 select UPPER(nome) as nome_maiusculo from nomes;
 
 select nome, LENGHT(nome) as tamanho from nomes;
+
+SELECT
+    CASE
+        WHEN nome LIKE 'João%' THEN 'Sr. '  nome
+        ELSE 'Sra. '  nome
+    END AS nome_com_tratamento
+FROM nomes;
