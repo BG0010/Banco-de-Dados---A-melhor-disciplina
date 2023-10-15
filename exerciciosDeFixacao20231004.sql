656f2b1f3e5f1983eb930230f69f41867f0729a8
Vinícius <borgesvini11@gmail.com>
2023-10-15 19:09:15 -0300

    Função para Obter a Média de Livros por Editora

--- exerciciosDeFixacao20231004.sql
+++ exerciciosDeFixacao20231004.sql
@@ -81,3 +81,26 @@ BEGIN
 END;
 //
 DELIMITER ;
+
+--Função para Obter a Média de Livros por Editora
+
+DELIMITER //
+CREATE FUNCTION media_livros_por_editora()
+RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE total_livros INT;
+    DECLARE total_editoras INT;
+    DECLARE media DECIMAL(10, 2);
+
+    SELECT COUNT(*) INTO total_editoras
+    FROM Editora;
+
+    SELECT SUM(editora_count.livros) INTO total_livros
+    FROM (SELECT id_editora, COUNT(*) AS livros FROM Livro GROUP BY id_editora) AS editora_count;
+
+    SET media = total_livros / total_editoras;
+
+    RETURN media;
+END;
+//
+DELIMITER ;
