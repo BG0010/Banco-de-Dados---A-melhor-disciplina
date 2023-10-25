c3592140e1c5913adb526f8f3ab12fb0e461b301
Borges <borgesvini11@gmial.com>
2023-10-25 20:49:21 -0300

    exercício 2

--- exerciciosDeFixacao20231025.sql
+++ exerciciosDeFixacao20231025.sql
@@ -5,3 +5,10 @@ for each row
 insert into Auditoria (mensagem)
 values(CONCAT('Um novo usuário foi adicionado', new.nome));
 --para ver se deu certo, adicione um nome na tabela Clientes.
+
+-- Antes de excluir um cliente da tabela Clientes, crie um trigger que insira uma mensagem na tabela Auditoria informando sobre a tentativa de exclusão
+
+create trigger excluir before delete on Clientes
+for each row
+insert into Auditoria (mensagem)
+values (CONCAT('O usuário deletou seu nome ', old.nome));
